f346a423129ed2039354559ca9fa332d4350fd41
vinuth <vinuthsriarampath@outlook.com>
2025-09-23 20:29:20 +0530

    Add custom error handling: implement AppError class and error middleware, and create an error route in app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 import express from "express";
 import routes from "./routes/index.js";
+import { errorHandler } from "./middlewares/errorMiddleware.js";
+import AppError from "./exceptions/appError.js";
 
 const app=express();
 
 app.use(express.json());
 app.use("/api",routes)
 
+// This route throws a custom error
+app.get('/api/error', (_req, _res, next) => {
+  next(new AppError(404, 'Resource not found in app.js'));
+});
+
+app.use(errorHandler)
 export default app;
