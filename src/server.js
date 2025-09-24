a4f449330c520fe7d035b46abbe845e8cc0bcdc9
vinuth <vinuthsriarampath@outlook.com>
2025-09-24 09:10:41 +0530

    Add dotenv dependency: include dotenv in package.json and package-lock.json, and update server.js to use lowercase 'port' variable for consistency

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from './app.js';
-const PORT = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}...`);
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}...`);
 })
